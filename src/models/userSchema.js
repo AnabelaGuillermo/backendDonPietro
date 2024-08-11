7909a564bbf3456636420deb1cd936e295fd7e48
Anabela Guillermo <anabela.guillermo@gmail.com>
2024-08-11 00:51:29 -0300

    Creo Schema de usuario, producto y pedidos

---
+++ src/models/userSchema.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+export default mongoose.model('User', UserSchema);
