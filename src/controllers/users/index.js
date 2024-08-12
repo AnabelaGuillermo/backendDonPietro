203df1f87d7352026842e9e2cc1689acafd221fe
nacho <nachosalpaz0@gmail.com>
2024-08-11 21:13:07 -0300

    creacion de los controladores y validaciones de usuarios

---
+++ src/controllers/users/index.js
@@ -0,0 +1,7 @@
+import { GetController } from './controllers/GetController.js';
+import { PostController } from './controllers/PostController.js';
+
+export const Users = {
+  GetController,
+  PostController,
+};
---
+++ src/helpers/validationSchemas/usersValidationSchemas.js
@@ -0,0 +1,28 @@
+import Joi from 'joi';
+
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+export const post_userValidationSchema = Joi.object({
+  fullname: Joi.string().trim().min(3).max(30).required().messages({
+    'string.min': "El campo 'fullname' debe tener como mínimo 3 caracteres",
+    'string.max': "El campo 'fullname' debe tener como máximo 30 caracteres",
+    'any.required': "El campo 'fullname' es requerido",
+    '*': "Revisa el campo 'fullname'",
+  }),
+  username: Joi.string().trim().min(3).max(15).required().messages({
+    'string.min': "El campo 'username' debe tener como mínimo 3 caracteres",
+    'string.max': "El campo 'username' debe tener como máximo 15 caracteres",
+    'any.required': "El campo 'username' es requerido",
+    '*': "Revisa el campo 'username'",
+  }),
+  email: Joi.string().trim().email().required().messages({
+    'string.email': "El campo 'email' debe ser una dirección de correo electrónico válida",
+    'any.required': "El campo 'email' es requerido",
+    '*': "Revisa el campo 'email'",
+  }),
+  password: Joi.string().trim().regex(passwordRegex).required().messages({
+    'string.pattern.base': "El campo 'password' debe tener una minúscula, una mayúscula, un dígito, y un caracter especial, entre 8 y 15 caracteres",
+    'any.required': "El campo 'password' es requerido",
+    '*': "Revisa el campo 'password'",
+  }),
+});
