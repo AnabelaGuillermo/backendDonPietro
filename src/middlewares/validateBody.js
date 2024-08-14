72c4c58f4c6151c983cd854c55a84cf94673745b
Santiago Puertas <santidoors13@gmail.com>
2024-08-13 23:13:28 -0300

    Cargo middlewares

---
+++ src/middlewares/validateBody.js
@@ -0,0 +1,18 @@
+import HttpCodes from 'http-status-codes';
+
+export const validateBody = (req, res, next, validationSchema) => {
+  const { body } = req;
+
+  const { error } = validationSchema.validate(body);
+
+  if (error) {
+    res.status(HttpCodes.BAD_REQUEST).json({
+      data: null,
+      message:
+        error.details[0].message || 'Ocurrió un error validando los campos',
+    });
+    return;
+  }
+
+  next();
+};
