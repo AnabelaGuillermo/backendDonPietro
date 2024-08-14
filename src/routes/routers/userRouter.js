5098df02a0fd094dd495e52d331e7c1aed2d116e
Santiago Puertas <santidoors13@gmail.com>
2024-08-13 23:44:38 -0300

    Cambio en las rutas de productRouter.js y userRouter.js

--- src/routes/routers/userRouter.js
+++ src/routes/routers/userRouter.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import { Users } from '../../controllers/users/index.js';
+import { validateBody } from '../../middlewares/validateBody.js';
+import { isAuthenticated } from '../../middlewares/isAuthenticated.js';
+import { isAdmin } from '../../middlewares/isAdmin.js';
+import { post_userValidationSchema } from '../../helpers/validationSchemas/usersValidationSchemas.js';
 
 export const userRouter = express.Router();
 // GET ----------------------------
-userRouter.get('/', Users.GetController.getUsers);
+userRouter.get('/', isAuthenticated, isAdmin, Users.GetController.getUsers);
 // POST ----------------------------
 // /api/v1/users/
-userRouter.post('/', Users.PostController.postUser);
+userRouter.post(
+  '/',
+  (req, res, next) => validateBody(req, res, next, post_userValidationSchema),
+  Users.PostController.postUser,
+);
