96fa2ac47341acdb8e09cfea1b7914484f9331fa
Santiago Puertas <santidoors13@gmail.com>
2024-08-21 19:30:23 -0300

    Se elimino todo lo relacionado al username

--- src/helpers/validationSchemas/authValidationSchemas.js
+++ src/helpers/validationSchemas/authValidationSchemas.js
@@ -1,13 +1,11 @@
 import Joi from 'joi';
 
 export const post_loginValidationSchema = Joi.object({
-  usernameOrEmail: Joi.string().trim().min(3).max(50).required().messages({
-    'string.min':
-      "El campo 'usernameOrEmail' debe tener como mínimo 3 caracteres",
-    'string.max':
-      "El campo 'usernameOrEmail' debe tener como máximo 50 caracteres",
-    'any.required': "El campo 'usernameOrEmail' es requerido",
-    '*': "Revisa el campo 'usernameOrEmail'",
+  email: Joi.string().trim().min(3).max(50).required().messages({
+    'string.min': "El campo 'email' debe tener como mínimo 3 caracteres",
+    'string.max': "El campo 'email' debe tener como máximo 50 caracteres",
+    'any.required': "El campo 'email' es requerido",
+    '*': "Revisa el campo 'email'",
   }),
   password: Joi.string().trim().min(8).required().messages({
     'string.min': "El campo 'password' debe tener al menos 8 caracteres",
--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -5,11 +5,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   email: {
     type: String,
     required: true,
