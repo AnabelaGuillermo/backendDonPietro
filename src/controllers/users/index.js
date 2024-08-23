fc3dc5e2d015ef0bd202692aaa8a09bb049d926f
Santiago Puertas <santidoors13@gmail.com>
2024-08-23 04:16:51 -0300

    Armado y terminado enrutado de passwordChange

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -2,10 +2,12 @@ import { DeleteController } from './controllers/DeleteController.js';
 import { GetController } from './controllers/GetController.js';
 import { PostController } from './controllers/PostController.js';
 import { AdminController } from './controllers/AdminController.js';
+import { PasswordController } from './controllers/PasswordController.js';
 
 export const Users = {
   GetController,
   PostController,
   DeleteController,
   AdminController,
+  PasswordController,
 };
